f88679137bb44f1f792f34c23f10dd4280c00cb3
Aldric Giacomoni <trevoke@gmail.com>
2017-08-14 10:21:35 -0400

    add documentation

--- lib/mock_behaviour.ex
+++ lib/mock_behaviour.ex
@@ -1,15 +1,102 @@
 defmodule MockBehaviour do
-  defmacro __using__(behaviour: behaviour) do
-    quote do
-      require unquote(__MODULE__)
-      import unquote(__MODULE__)
+  @moduledoc """
+  MockBehaviour helps you maintain mocks for your behaviours.
+
+  This code is inspired by the pattern shown in Jose Valim's article on mocks ( http://blog.plataformatec.com.br/2015/10/mocks-and-explicit-contracts/ ).
+
+  Assume the following behaviour:
+
+  ```elixir
+  defmodule TwitterContract do
+    @callback tweets(user_id :: String.t) :: [map()]
+  end
+
+  defmodule TwitterContractHttp do
+    @behaviour TwitterContract
+    def tweets(user_id), do: # call to Twitter, return desired tweets
+  end
+  ```
+
+  At this point, your code might be using `TwitterContractHttp` and you are now inextricably bound to an external service.
+
+  Not so! You can create an API to get to your external services, and use mocks at that level. All you have to do is this:
+
+  ```elixir
+  defmodule Contracts do
+    @behaviour TwitterContract
+    @twitter Application.get_env(:app, :contracts)[:twitter_module]
+
+    def tweets(user_id), do: @twitter.tweets(user_id)
+  end
+  ```
+
+  And you can configure your system like so:
+
+  ```elixir
+  # dev
+  config :app, :contracts, [
+    twitter_module: TwitterContractHttp
+  ]
+
+  # test
+  config :app, :contracts, [
+    twitter_module: TwitterContractMock
+  ]
+  ```
+
+  At this point, to reap the rewards from this package, all you have to do is:
+
+  ```elixir
+  defmodule TwitterContractMock do
+    use MockBehaviour, behaviour: TwitterContract
+  end
+  ```
+
+  This will generate the following code:
+
+  ```elixir
+  defmodule TwitterContractMock do
+    use GenServer
+    def start_link(state) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
     end
-    x = Macro.expand(behaviour, __ENV__).behaviour_info(:callbacks)
-    for {fname, arity} <- x do
-      quote do
-        MockBehaviour.handle_mock_call(unquote(fname), unquote(arity))
+
+    def tweets(user_id) do
+      GenServer.call(__MODULE__, {:tweets, user_id})
+    end
+
+    def handle_call({:tweets, user_id}, _from, state) do
+      response = state.tweets.(user_id)
+      {:reply, response, state}
+    end
+  end
+  ```
+
+  From there, in your tests you can simply define and use an anonymous function:
+
+  ```elixir
+  tweets = fn(x) -> [%{tweet: "This package is such a timesaver", user: "Trevoke" }]
+
+  TwitterContractMock.start_link(%{tweets: tweets})
+  ```
+
+  Enjoy your self-maintaining mocks!
+  """
+  defmacro __using__(behaviour: behaviour) do
+    a = quote do
+      use GenServer
+      IO.inspect __MODULE__
+      def start_link(state) do
+        GenServer.start_link(__MODULE__, state, name: __MODULE__)
       end
     end
+    x = Macro.expand(behaviour, __ENV__).behaviour_info(:callbacks)
+    b = for {fname, arity} <- x do
+      quote do
+        Foo.handle_mock_call(unquote(fname), unquote(arity))
+      end
+    end
+    [a, b]
   end
 
   defmacro handle_mock_call(x, y) when is_atom(x) and is_integer(y) do
--- mix.exs
+++ mix.exs
@@ -2,12 +2,23 @@ defmodule MockBehaviour.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mock_behaviour,
-     version: "0.1.0",
-     elixir: ">= 1.4.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :mock_behaviour,
+      version: "0.1.0",
+      elixirc_paths: elixirc_paths(Mix.env),
+      elixir: ">= 1.4.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      name: "MockBehaviour",
+      description: "Generates mocks for your behaviours",
+      source_url: "https://github.com/trevoke/mock_behaviour",
+      package: package(),
+      deps: deps(),
+      docs: [
+        main: "MockBehaviour",
+        source_url: "https://github.com/trevoke/mock_behaviour"
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -18,16 +29,20 @@ defmodule MockBehaviour.Mixfile do
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib",]
+
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [description: "Generates mocks for your behaviours",
+     files: ["lib", "config", "mix.exs", "README*"],
+     maintainers: ["Aldric Giacomoni"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/trevoke/mock_behaviour"}]
   end
 end
