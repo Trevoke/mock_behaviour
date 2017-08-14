f0fcc7543bc9bebee376c2b3ef3a0b3ea0ca39f2
Aldric Giacomoni <trevoke@gmail.com>
2017-08-14 10:24:30 -0400

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MockBehaviour.Mixfile do
   def project do
     [
       app: :mock_behaviour,
-      version: "0.1.0",
+      version: "0.1.1",
       elixirc_paths: elixirc_paths(Mix.env),
       elixir: ">= 1.4.0",
       build_embedded: Mix.env == :prod,
